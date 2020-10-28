d2d67d99547c149258367ed7898de55d3bcc6573
kyhslam <kyhslam@gmail.com>
2020-10-28 18:01:15 +0900

    init

---
+++ src/main/java/org/kyhslam/Member.java
@@ -0,0 +1,33 @@
+package org.kyhslam;
+
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
+
